83d96ddd7cd2e733b7a67e67d8f9e735683f3f1c
Kyle Gorkowski <kkgorkow@gmail.com>
2023-03-26 11:49:15 -0600

    added more conversions

--- datacula/convert.py
+++ datacula/convert.py
@@ -2,7 +2,7 @@
 """
 
 import numpy as np
-from typing import Union
+from typing import Union, Tuple
 
 
 def coerce_type(data, dtype):
@@ -30,23 +30,19 @@ def round(
     """
     Rounds the input values to the nearest multiple of the base.
 
-    For values exactly halfway between rounded decimal values, "Breakers
+    For values exactly halfway between rounded decimal values, "Bankers
     rounding applies" rounds to the nearest even value. Thus 1.5 and 2.5
     round to 2.0, -0.5 and 0.5 round to 0.0, etc.
 
     Parameters:
-        values (Union[float, List[float], np.ndarray])
-            The values to be rounded.
-        base (float): default 1.0
-            The base to which the values should be rounded (default 1.0).
-        mode (str): default 'round'
-            The rounding mode: 'round', 'floor', 'ceil'
-        nonzero_edge: False
-            If true the zero values are replaced by the original values.
+        values: The values to be rounded.
+        base: The base to which the values should be rounded.
+        mode: The rounding mode: 'round', 'floor', 'ceil'
+        nonzero_edge: If true the zero values are replaced
+        by the original values.
 
     Returns:
-        Union[float, List[float]]
-            The rounded values.
+        rounded: The rounded values.
     """
     # Check if values is a NumPy array
     values = coerce_type(values, np.ndarray)
@@ -79,24 +75,255 @@ def round(
     return rounded.tolist() if isinstance(values, list) else rounded
 
 
-# def round(values, base=1.0, mode: str = 'round') -> float:
-#         """
-#         Rounds to the floor a number to the nearest base.
-#         """
-#         # check np.array
-#         if type(values) is np.ndarray:
-#             pass
-#         else:
-#             values = np.array(values)
+def radius_diameter(value: float, to_diameter: bool = True) -> float:
+    """
+    Convert a radius to a diameter, or vice versa.
 
-#         if mode == 'round':
-#             return base * np.round(values/base)
-#         elif mode == 'floor':
-#             return base * np.round(values/base-0.5)
-#         elif mode == 'ceil':
-#             return base * np.round(values/base+0.5)
-#         elif mode == 'round_nonzero':
-#             rounded = base * np.round(values/base)
-#             return np.where(rounded != 0, rounded, values).tolist()
-#         else:
-#             raise ValueError('mode must be round, floor or ceil')
+    Parameters:
+        value: The value to be converted.
+        to_diameter: If True, convert from radius to diameter.
+        If False, convert from diameter to radius.
+
+    Returns:
+        The converted value.
+    """
+    if to_diameter:
+        return value * 2
+    else:
+        return value / 2
+
+
+def volume_to_length(volume: float, length_type: str = 'radius') -> float:
+    """
+    Convert a volume to a radius or diameter.
+
+    Parameters:
+        volume: The volume to be converted.
+        length_type: The type of length to convert to ('radius' or 'diameter') Default is 'radius'.
+
+    Returns:
+        The converted length.
+    """
+    radius = (volume * 3 / (4 * np.pi)) ** (1 / 3)
+    if length_type == 'radius':
+        return radius
+    elif length_type == 'diameter':
+        return radius * 2
+    else:
+        raise ValueError('length_type must be radius or diameter')
+
+
+def length_to_volume(length: float, length_type: str = 'radius') -> float:
+    """
+    Convert radius or diameter to volume.
+
+    Parameters:
+        length: The length to be converted.
+        length_type: The type of length ('radius' or 'diameter').
+            Default is 'radius'.
+
+    Returns:
+        The volume.
+    """
+    if length_type == 'diameter':
+        length = length / 2
+    elif length_type == 'radius':
+        pass
+    else:
+        raise ValueError('length_type must be radius or diameter')
+    return (4/3)*np.pi*(length**3)
+
+
+def kappa_volume_solute(
+            volume_total: float,
+            kappa: float,
+            water_activity: float
+        ) -> np.ndarray:
+    """
+    Calculate the volume of solute in a volume of total solution,
+    given the kappa parameter and water activity.
+
+    Parameters:
+        volume_total: The volume of the total solution.
+        kappa: The kappa parameter.
+        water_activity: The water activity.
+
+    Returns:
+        The volume of solute as a numpy array.
+    """
+    vol_factor = (water_activity - 1) / (
+            water_activity * (1 - kappa - 1 / water_activity)
+        )
+    return volume_total * np.array(vol_factor)
+
+
+def kappa_volume_water(
+            volume_solute: float,
+            kappa: float,
+            water_activity: float
+        ) -> float:
+    """
+    Calculate the volume of water given volume of solute, kappa parameter,
+    and water activity.
+
+    Parameters:
+        volume_solute: The volume of solute.
+        kappa: The kappa parameter.
+        water_activity: The water activity.
+
+    Returns:
+        The volume of water as a float.
+    """
+    return volume_solute * kappa / (1 / water_activity - 1)
+
+
+def kappa_from_volume(
+            volume_solute: float,
+            volume_water: float,
+            water_activity: float
+        ) -> float:
+    """
+    Calculate the kappa parameter from the volume of solute and water,
+    given the water activity.
+
+    Parameters:
+        volume_solute: The volume of solute.
+        volume_water: The volume of water.
+        water_activity: The water activity.
+
+    Returns:
+        The kappa parameter as a float.
+    """
+    return (1 / water_activity - 1) * volume_water / volume_solute
+
+
+def mole_fraction_to_mass_fraction(
+        mole_fraction0: float,
+        molecular_weight0: float,
+        molecular_weight1: float
+        ) -> Tuple[float, float]:
+    """
+    Convert mole fraction to mass fraction.
+
+    Parameters:
+        mole_fraction0: The mole fraction of the first component.
+        molecular_weight0: The molecular weight of the first component.
+        molecular_weight1: The molecular weight of the second component.
+
+    Returns:
+        A tuple containing the mass fractions of the two components as floats.
+    """
+    mass_fraction0 = mole_fraction0 * molecular_weight0 / (
+            mole_fraction0 * molecular_weight0
+            + (1 - mole_fraction0) * molecular_weight1
+        )
+    mass_fraction1 = 1 - mass_fraction0
+    return mass_fraction0, mass_fraction1
+
+
+def mole_fraction_to_mass_fraction_multi(
+            mole_fractions: list[float],
+            molecular_weights: list[float]
+        ) -> list[float]:
+    """Convert mole fractions to mass fractions for N components.
+    Assumes that sum(mole_fractions) == 1.
+
+    Parameters:
+        mole_fractions: A list of mole fractions.
+        molecular_weights: A list of molecular weights.
+
+    Returns:
+        A list of mass fractions.
+    """
+    total_molecular_weight = np.sum(
+            [mf * mw for mf, mw in zip(mole_fractions, molecular_weights)]
+        )
+    return [
+            mf * mw / total_molecular_weight for mf,
+            mw in zip(mole_fractions, molecular_weights)
+        ]
+
+
+def mass_fraction_to_volume_fraction(
+    mass_fraction0: float, density0: float, density1: float
+) -> Tuple[float, float]:
+    """Convert mass fraction to volume fraction"""
+    volume_fraction0 = (mass_fraction0 / density0) / (
+        mass_fraction0 / density0 + (1 - mass_fraction0) / density1
+    )
+    volume_fraction1 = 1 - volume_fraction0
+    return volume_fraction0, volume_fraction1
+
+
+def volume_water_from_volume_fraction(
+    volume_solute_dry: float, 
+    volume_fraction_water: float
+) -> float:
+    """
+    Calculate the volume of water in a volume of solute,
+    given the volume fraction of water.
+    """
+    return volume_fraction_water * volume_solute_dry / (
+        1 - volume_fraction_water
+    )
+
+
+def effective_refractive_index(
+            m_zero: float,
+            m_one: float,
+            volume_zero: float,
+            volume_one: float
+        ) -> float:
+    """Calculate the refractive index of a mixture of two solutes,
+    given the refractive index of each solute and the volume of each solute.
+    The volume can be volume fractions.
+    The mixing is based on volume weight molar refraction.
+
+    Parameters:
+        m0: Refractive index of solute 0.
+        m_one: Refractive index of solute 1.
+        volume0: Volume of solute 0.
+        volume_one: Volume of solute 1.
+
+    Returns:
+        The effective refractive index of the mixture.
+
+    Reference:
+        Liu, Y., &#38; Daum, P. H. (2008).
+        Relationship of refractive index to mass density and self-consistency
+        mixing rules for multicomponent mixtures like ambient aerosols.
+        Journal of Aerosol Science, 39(11), 974-986.
+        https://doi.org/10.1016/j.jaerosci.2008.06.006
+    """
+    volume_total = volume_zero + volume_one
+    r_effective = (
+        volume_zero/volume_total * (m_zero-1)/(m_zero+2)
+        + volume_one/volume_total * (m_one-1)/(m_one+2)
+        )  # molar refraction mixing
+    return (2*r_effective +1)/(1-r_effective)  # convert to refractive index
+
+
+def convert_sizer_dn(
+            diameter: np.ndarray,
+            dn_dlogdp: np.ndarray
+        ) -> np.ndarray:
+    """
+    Converts the sizer data from dn/dlogdp to dn
+    TODO: fix the over counting of the last bin or first?
+
+    Parameters:
+        diameter : diameter array
+        dn_dlogdp : dn/dlogdp array
+
+    Returns:
+        dn array
+    """
+
+    delta = np.zeros_like(diameter)
+    delta[:-1] = np.diff(diameter)
+    delta[-1] = delta[-2]**2/delta[-3]
+
+    lower = diameter-delta/2
+    upper = diameter+delta/2
+
+    return dn_dlogdp*np.log10(upper/lower)
