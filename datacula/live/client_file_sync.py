b6a454ef07adfd095e37921cb553d3094b884d14
Kyle Gorkowski <kkgorkow@gmail.com>
2023-03-26 19:50:30 -0600

    worked on file sync

--- datacula/live/client_file_sync.py
+++ datacula/live/client_file_sync.py
@@ -1,19 +1,23 @@
-#%%
+"""File syncing script for a client to copy local files to a network drive.
 
-# -*- coding: utf-8 -*-
+Writen to work independently of the rest of the datacula code base and run on
+older versions of python.
 """
-Created by Kyle Gorkowski
-"""
-# import socket
-from distutils import core
+
 import time
-# import glob
 import os
 import json
 import file_sync
+import time
+import logging
+import datetime
+from scheduler import Scheduler
 
+schedule = Scheduler()
+
+logging.basicConfig(level=logging.INFO)
 script_path = os.path.dirname(__file__)
-# print(script_path)
+
 
 def file_sync_settings_dict_creator() -> dict:
     """
@@ -23,30 +27,35 @@ def file_sync_settings_dict_creator() -> dict:
         "blank_data": {
             "core_folder": script_path,
             "clone_folder": script_path,
-            "files_core_search_key":'*.*',
-            "files_clone_search_key":'*.*', 
+            "files_core_search_key": '*.*',
+            "files_clone_search_key": '*.*',
             "max_file_age_days": 30,
         },
         "blank_data2": {
             "core_folder": script_path,
             "clone_folder": script_path,
-            "files_core_search_key":'*.*',
-            "files_clone_search_key":'*.*', 
+            "files_core_search_key": '*.*',
+            "files_clone_search_key": '*.*',
             "max_file_age_days": 30,
         },
     }
     return file_sync_settings_dict
 
 
-def write_file_sync_settings(file_sync_settings :dict ) -> None:
+def write_file_sync_settings(
+            sync_settings: dict
+        ) -> None:
     """
     Writes server data settings to json file that is located in the same folder 
     as this script.
     """
-    with open(os.path.join(script_path,'file_sync_settings.json'), 'w',
-        encoding='utf-8'
-    ) as json_file:
-        json.dump(file_sync_settings, json_file, indent=4)
+    with open(
+                os.path.join(script_path, 'file_sync_settings.json'),
+                'w',
+                encoding='utf-8'
+            ) as json_file:
+
+        json.dump(sync_settings, json_file, indent=4)
 
 
 def get_file_sync_settings(mode: str = 'automatic') -> dict:
@@ -63,18 +72,20 @@ def get_file_sync_settings(mode: str = 'automatic') -> dict:
     """
     if mode == 'automatic':
         try:
-            with open(os.path.join(script_path,'file_sync_settings.json'), 
-            'r', encoding='utf-8'
-            ) as json_file:
+            with open(
+                        os.path.join(script_path, 'file_sync_settings.json'),
+                        'r',
+                        encoding='utf-8'
+                    ) as json_file:
                 file_sync_settings = json.load(json_file)
+
         except FileNotFoundError:
             file_sync_settings = file_sync_settings_dict_creator()
             write_file_sync_settings(file_sync_settings)
     elif mode == 'new':
-            file_sync_settings = file_sync_settings_dict_creator()
-            write_file_sync_settings(file_sync_settings)
+        file_sync_settings = file_sync_settings_dict_creator()
+        write_file_sync_settings(file_sync_settings)
     return file_sync_settings
-
 
 
 # Run main function
@@ -85,36 +96,87 @@ def main():
     file_sync_settings = get_file_sync_settings()
 
     for file_prop in file_sync_settings:
-        # print(file_sync_settings[file_prop]['core_folder'])
         print('Checking '+file_prop)
         file_sync.copy_files(
-            core_directory=file_sync_settings[file_prop]['core_folder'],
-            clone_directory=file_sync_settings[file_prop]['clone_folder'],
-            core_search_key=file_sync_settings[file_prop]['files_core_search_key'],
-            clone_search_key=file_sync_settings[file_prop]['files_clone_search_key'],
-            max_file_age_days=file_sync_settings[file_prop]['max_file_age_days'],
+            core_directory=file_sync_settings[file_prop][
+                'core_folder'],
+            clone_directory=file_sync_settings[file_prop][
+                'clone_folder'],
+            core_search_key=file_sync_settings[file_prop][
+                'files_core_search_key'],
+            clone_search_key=file_sync_settings[file_prop][
+                'files_clone_search_key'],
+            max_file_age_days=file_sync_settings[file_prop][
+                'max_file_age_days'],
             )
 
-#%%
+
+def run_file_sync():
+    try:
+        logging.info('Running file sync at %s', datetime.datetime.now())
+        main()
+    except PermissionError as e:
+        logging.error('Permission error: %s', str(e))
+    except Exception as e:
+        logging.error('File sync error: %s', str(e))
+
+
+def main():
+    file_sync_settings = get_file_sync_settings()
+    for prop_name, prop_data in file_sync_settings.items():
+        logging.info('Checking %s', prop_name)
+        file_sync.copy_files(
+            core_directory=prop_data['core_folder'],
+            clone_directory=prop_data['clone_folder'],
+            core_search_key=prop_data['files_core_search_key'],
+            clone_search_key=prop_data['files_clone_search_key'],
+            max_file_age_days=prop_data['max_file_age_days'],
+        )
+
+
 if __name__ == '__main__':
     file_sync_settings = get_file_sync_settings()
-    if list(file_sync_settings.keys())[0] != 'blank_data':
-        print('Startup Check')
-        main()
-        while True:
-            if any(x==time.localtime()[3] for x in [2,4,6,8,10,12,14,16,18,20,22]):
-                time.sleep(30)# delay offset
-                try:
-                    print('running file sync at '+time.ctime())
-                    main()
-                    time.sleep(3600) # sleep for an hour
-                except:
-                    print('[****file sync error, trying again in 15 sec]')
-                    time.sleep(15)
-            else:
-                time.sleep(15) # sleep between hour checks
-    else:
+    if list(file_sync_settings.keys())[0] == 'blank_data':
         print('Created the parameter file, you need to edit it and re-run')
+    else:
+        logging.info('Startup check')
+        main()
+        # https://digon.io/hyd/project/scheduler/t/master/pages/examples/quick_start.html
+        # Schedule file sync to run every two hours between 2am and 10pm
+        schedule.every(2).hours.at('XX:05').do(run_file_sync)
+        # schedule.every(2).hours.at('04:00').do(run_file_sync)
+        # schedule.every(2).hours.at('06:00').do(run_file_sync)
+        # schedule.every(2).hours.at('08:00').do(run_file_sync)
+        # schedule.every(2).hours.at('10:00').do(run_file_sync)
+        # schedule.every(2).hours.at('12:00').do(run_file_sync)
+        # schedule.every(2).hours.at('14:00').do(run_file_sync)
+        # schedule.every(2).hours.at('16:00').do(run_file_sync)
+        # schedule.every(2).hours.at('18:00').do(run_file_sync)
+        # schedule.every(2).hours.at('20:00').do(run_file_sync)
+
+
+
+
+# if __name__ == '__main__':
+#     file_sync_settings = get_file_sync_settings()
+
+#     if list(file_sync_settings.keys())[0] != 'blank_data':
+#         print('Startup Check')
+#         main()
+#         while True:
+#             if any(x==time.localtime()[3] for x in [2,4,6,8,10,12,14,16,18,20,22]):
+#                 time.sleep(30)# delay offset
+#                 try:
+#                     print('running file sync at '+time.ctime())
+#                     main()
+#                     time.sleep(3600) # sleep for an hour
+#                 except:
+#                     print('[****file sync error, trying again in 15 sec]')
+#                     time.sleep(15)
+#             else:
+#                 time.sleep(15) # sleep between hour checks
+#     else:
+#         print('Created the parameter file, you need to edit it and re-run')
 
 
 # %%
--- datacula/live/file_sync.py
+++ datacula/live/file_sync.py
@@ -1,121 +1,116 @@
-# -*- coding: utf-8 -*-
 """
-Created on July 16 2022
 Copies data from core folder (or path) to clone path (or folder)
-Methods:
-    copy_files(core_directory:str, clone_directory:str,
-        core_search_key:str = '*.*', clone_search_key:str = '*.*',
-        )->None
-    copy_folders(core_path:str, clone_path:str,
-        core_search_key:str = '*', clone_search_key:str = '*',)->None
-
-@author: Kyle Gorkowski
+    Methods: copy_files, copy_folders
 """
 import glob
 import os
-import platform
 import time
 
-if platform.system() == 'Windows':
-    system_copy = 'copy'
-else:
-    system_copy = 'cp'
 
-
-def copy_files(core_directory:str, clone_directory:str,
-    core_search_key:str = '*.*', clone_search_key:str = '*.*',
-    max_file_age_days:float = -1,
-    )->None:
+def copy_files(
+            core_directory: str,
+            clone_directory: str,
+            core_search_key: str = '*.*',
+            clone_search_key: str = '*.*',
+            max_file_age_days: float = -1
+        ) -> None:
     """
-    Copies file from the core directory to the clone directory, with a check 
-    for file size. If the file size is the same, the file is not copied. 
-    If the file size is different, the file is copied. If the file does not 
+    Copies files from the core directory to the clone directory, with a check
+    for file size. If the file size is the same, the file is not copied.
+    If the file size is different, the file is copied. If the file does not
     exist in the clone directory, it is copied.
 
     Parameters:
-        core_dir (str): The directory to copy files from.
-        clone_dir (str): The directory to copy files to.
-        core_search_key (str): glob.glob search key to use
-        clone_search_key (str): glob.glob search key to use
+        core_directory (str): The directory to copy files from.
+        clone_directory (str): The directory to copy files to.
+        core_search_key (str): glob.glob search key to use.
+        clone_search_key (str): glob.glob search key to use.
+        max_file_age_days (float): Maximum age of files to copy (in days).
     """
-    if os.path.exists(core_directory):
-        core_dir_files = glob.glob(os.path.join(core_directory,core_search_key))
-    else:
+    if not os.path.exists(core_directory):
         print('******Core directory does not exist******')
+        return
+    print(f"    core path {core_directory}")
+    print(f" -->clone path {clone_directory}")
+
+    core_dir_files = glob.glob(os.path.join(core_directory, core_search_key))
 
     if os.path.exists(clone_directory):
         clone_dir_files = glob.glob(
-            os.path.join(clone_directory, clone_search_key)
-            )
+            os.path.join(clone_directory, clone_search_key))
     else:
         os.makedirs(clone_directory)
-        clone_dir_files = glob.glob(
-            os.path.join(clone_directory, clone_search_key)
-            )
+        clone_dir_files = []
 
     max_file_age_sec = max_file_age_days * 86400
-    #loop through all files in core_dir and check if they exist in clone_dir
+    system_copy = 'cp' if os.name == 'posix' else 'copy'
+
+    # Loop through all files in core_dir and check if they exist in clone_dir
     for file in core_dir_files:
         file_name = os.path.basename(file)
 
+        # Check if the file is too old or copy all files
+        if max_file_age_days < 0 or (
+                    os.path.getctime(file) + max_file_age_sec < time.time()
+                ):
+            continue  # skips to next loop iteration
         copy = True
-        #checks if the file is too old
-        if os.path.getctime(file)+max_file_age_sec > time.time() or max_file_age_days < 0 :
 
-            for file_clone_check in clone_dir_files:
-                file_clone_name = os.path.basename(file_clone_check)
-                if file_name == file_clone_name:
-                    #file exists in clone_dir, check size
-                    if os.path.getsize(file) == os.path.getsize(file_clone_check):
-                        copy = False
-                        clone_dir_files.remove(file_clone_check) #removes file from list
-                    break
-            if copy:
-                os.system(
-                    system_copy+ ' "'+file+ '"'+' '+ '"'+
-                    os.path.join(clone_directory,file_name)+'"'
-                    )
-                print('    copied: ' + file_name)
+        # Check if the file exists in clone_dir and has the same size
+        for file_clone_check in clone_dir_files:
+            file_clone_name = os.path.basename(file_clone_check)
+
+            if file_name == file_clone_name:
+                if os.path.getsize(file) == os.path.getsize(file_clone_check):
+                    copy = False
+                    clone_dir_files.remove(file_clone_check)  # Removes file
+                break
+
+        if copy:
+            new_clone = os.path.join(clone_directory, file_name)
+            os.system(f"{system_copy} '{file}' '{new_clone}'")
+
+            print(f"    copied: {file_name}")
 
 
-# core_dir = 'F:\\OneDrive\\Professional Current DnD\\Aerosol microphysics\\CAFE-processing\\server\\server_files\\NOx_data'
-# clone_dir = 'F:\\OneDrive\\Professional Current DnD\\Aerosol microphysics\\CAFE-processing\\server\\server_files_clone\\NOx_data'
-# copy_files(core_dir, clone_dir)
-
-#copy folders from core to clone
-def copy_folders(core_path:str, clone_path:str,
-    core_search_key:str = '*', clone_search_key:str = '*', 
-    files_core_search_key:str = '*.*', files_clone_search_key:str = '*.*', 
-    max_file_age_days:float = -1,
-    )->None:
+def copy_folders(
+            core_path: str,
+            clone_path: str,
+            core_search_key: str = '*',
+            files_core_search_key: str = '*.*',
+            files_clone_search_key: str = '*.*',
+            max_file_age_days: float = -1
+        ) -> None:
     """
     Copies folders from the core directory to the clone directory, with a check
     for files size. If the file size is the same, the file is not copied.
-    
+
     Parameters:
         core_path (str): The directory to copy folders from.
         clone_path (str): The directory to copy folders to.
-        core_search_key (str): glob.glob search key to use
-        clone_search_key (str): glob.glob search key to use
+        core_search_key (str): glob.glob search key to use for folders.
+        files_core_search_key (str): glob.glob search key to use for files.
+        files_clone_search_key (str): glob.glob search key to use for files.
+        max_file_age_days (float): Maximum age of files to copy (in days).
     """
 
-    if os.path.exists(core_path):
-        core_path_folders = glob.glob(os.path.join(core_path,core_search_key))
-    else:
+    if not os.path.exists(core_path):
         print('******Core path does not exist******')
+        return
+
+    core_path_folders = glob.glob(os.path.join(core_path, core_search_key))
 
     for folder in core_path_folders:
         folder_name = os.path.basename(folder)
         print('Compared folder: ' + folder_name)
 
-        copy_files(folder, os.path.join(clone_path, folder_name),
-        core_search_key = files_core_search_key, clone_search_key = files_clone_search_key,
-        max_file_age_days = max_file_age_days)
+        clone_folder = os.path.join(clone_path, folder_name)
+        os.makedirs(clone_folder, exist_ok=True)
 
-
-# # %%
-
-# core_path = 'F:\\OneDrive\\Professional Current DnD\\Aerosol microphysics\\CAFE-processing\\server\\server_files'
-# clone_path = 'F:\\OneDrive\\Professional Current DnD\\Aerosol microphysics\\CAFE-processing\\server\\server_files_clone'
-
-# copy_folders(core_path, clone_path)
+        copy_files(
+            core_directory=folder,
+            clone_directory=clone_folder,
+            core_search_key=files_core_search_key,
+            clone_search_key=files_clone_search_key,
+            max_file_age_days=max_file_age_days
+        )
